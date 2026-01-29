2c5bb6868c02ecda11b5350536dad51ea1bb3ffa
Danielivanleon <99567935+Danielivanleon@users.noreply.github.com>
2026-01-29 23:44:54 +0000

    se edito  la migración de Ponentes

--- src/database/migrations/2026_01_29_233939_create_ponentes_table.php
+++ src/database/migrations/2026_01_29_233939_create_ponentes_table.php
@@ -1,27 +1,10 @@
-<?php
-
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+public function up()
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('ponentes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('ponentes');
-    }
-};
+    Schema::create('ponentes', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->text('biografia')->nullable();
+        $table->string('especialidad')->nullable();
+        $table->timestamps();
+    });
+}
