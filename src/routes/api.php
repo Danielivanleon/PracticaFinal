d560c951c676eca91dd080e477111a59241869ee
Danielivanleon <99567935+Danielivanleon@users.noreply.github.com>
2026-01-29 22:32:44 +0000

    se edito el archivo routers api

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventoController;
+
+
+Route::get('/eventos', [EventoController::class, 'index']);
+Route::post('/eventos', [EventoController::class, 'store']);
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+Route::put('/eventos/{id}', [EventoController::class, 'update']);
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
