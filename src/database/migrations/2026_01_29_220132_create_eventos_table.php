6d398fc8f2a5bb16c05de124bc8641272bf48ef5
Danielivanleon <99567935+Danielivanleon@users.noreply.github.com>
2026-01-29 22:05:12 +0000

    se edito la tabla eventos

--- src/database/migrations/2026_01_29_220132_create_eventos_table.php
+++ src/database/migrations/2026_01_29_220132_create_eventos_table.php
@@ -9,13 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('eventos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+   public function up()
+{
+    Schema::create('eventos', function (Blueprint $table) {
+        $table->id();
+        $table->string('titulo');
+        $table->text('descripcion')->nullable();
+        $table->date('fecha_inicio')->nullable();
+        $table->date('fecha_fin')->nullable();
+        $table->string('ubicacion')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
