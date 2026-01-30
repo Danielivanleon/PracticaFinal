09852791acdc558eef5da36811d2db768335fb74
Danielivanleon <99567935+Danielivanleon@users.noreply.github.com>
2026-01-30 00:07:32 +0000

    se modifico metodo up de ponente

--- src/database/migrations/2026_01_30_000416_create_ponentes_table.php
+++ src/database/migrations/2026_01_30_000416_create_ponentes_table.php
@@ -9,13 +9,17 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('ponentes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('ponentes', function (Blueprint $table) {
+        $table->id();
+        $table->string('nombre');
+        $table->text('biografia')->nullable();
+        $table->string('especialidad')->nullable();
+        $table->timestamps();
+    });
+}
+
 
     /**
      * Reverse the migrations.
