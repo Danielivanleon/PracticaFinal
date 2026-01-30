975c2350d140da409f373e7be05242118b260160
Danielivanleon <99567935+Danielivanleon@users.noreply.github.com>
2026-01-30 00:53:56 +0000

    se agregaron nueva rutas en web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,4 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::post('/ponentes-vista',[PonenteVistaController::class,'store'])->name('ponentes.store');
+Route::delete('/ponentes-vista/{id}', [PonenteVistaController::class, 'destroy'])->name('ponentes.destroy');
+
 });
